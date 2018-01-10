b0eab5368d79486631d6bb681a0c9190094c33ce
CMontanha <35295764+CMontanha@users.noreply.github.com>
2018-01-10 11:35:40 +0000

    Update SpreadCheck.py

--- SpreadCheck.py
+++ SpreadCheck.py
@@ -5,53 +5,63 @@ import json
 from time import gmtime, strftime
 
 
-market = input("Market: ")
+AmountResults = 1
+N = 1
+StackValue = 0.001
+
+Market = input("Currency to search: ")
+marketSell1 = "BTC-ETH"
+marketSell2 = "ETH-" + Market
+marketBuy = "BTC-" + Market
 
 url = "https://bittrex.com/api/v1.1/public/getorderbook"
 
 try:
  while True:
   #absolutely_unused_variable = os.system("cls")
-
-  #BTC/ETH
-  #------------------------------------------------------------------------------------------------
-  parameters = {"market": "BTC-ETH", "type": "sell"}
-  response = requests.get(url, params = parameters)
-  data = response.json()
-  Quantity1 = data['result'][0]['Quantity']
-  Rate1 = data['result'][0]['Rate']
-
-
-  #ETH/MARKET
-  #------------------------------------------------------------------------------------------------
-  parameters = {"market": "ETH-" + market , "type": "sell"}
-  response = requests.get(url, params = parameters)
-  data = response.json()
-  Quantity2 = data['result'][0]['Quantity']
-  Rate2 = data['result'][0]['Rate']
-
-  #BTC/MARKET
+  
+  #Requests
   #-------------------------------------------------------------------------------------------------
-  parameters = {"market": "BTC-" + market , "type": "buy"}
-  response = requests.get(url, params = parameters)
-  data = response.json()
-  Quantity3 = data['result'][0]['Quantity']
-  Rate3 = data['result'][0]['Rate']
+  parameters1 = {"market": marketSell1, "type": "sell"}
+  response1 = requests.get(url, params = parameters1)
 
-  #SpreadCalculator
+  parameters2 = {"market": marketSell2, "type": "sell"}
+  response2 = requests.get(url, params = parameters2)
+
+  parameters3 = {"market": marketBuy, "type": "buy"}
+  response3 = requests.get(url, params = parameters3)
+
+  data1 = response1.json()
+  data2 = response2.json()
+  data3 = response3.json()
+
+  #BTC/ETH (Sell Order)
+  #------------------------------------------------------------------------------------------------
+  for x in range(0, AmountResults):
+    Quantity1 = data1['result'][x]['Quantity']
+    Rate1 = data1['result'][x]['Rate']
+
+  #ETH/COIN (Sell Order)
+  #------------------------------------------------------------------------------------------------
+    Quantity2 = data2['result'][x]['Quantity']
+    Rate2 = data2['result'][x]['Rate']
+
+  #BTC/COIN (Buy Order)
+  #-------------------------------------------------------------------------------------------------
+    Quantity3 = data3['result'][x]['Quantity']
+    Rate3 = data3['result'][x]['Rate']
+
+  #Spread
   #--------------------------------------------------------------------------------------------------
-  N = 1
-  Spread = ((N*Rate3*100)/(N*Rate2*Rate1))
+    Spread = ((N*Rate3*100)/(N*Rate2*Rate1))
   
-  #Condition (Volume 1st Orders)
+  #Condition Volume 1st Order
   #--------------------------------------------------------------------------------------------------
-  StackValue = 0.001
-  TestCondition1 = (StackValue <= Rate1*Quantity1) and (StackValue <= Rate2*Quantity2) and (StackValue <= Rate3*Quantity3)
-  
-  print(market, " ", Spread, " ", TestCondition1, " ", strftime("%d-%m-%Y %H:%M:%S", gmtime()))
-  f = open(market+".txt", 'a')
-  f.write(market + " " + str(Spread) + " " + str(TestCondition1) + " " + strftime("%d-%m-%Y %H:%M:%S", gmtime()) + "\n")
-  f.close()
+    TestCondition1 = (StackValue <= Rate1*Quantity1) and (StackValue <= Rate2*Quantity2) and (StackValue <= Rate3*Quantity3)
+    print(Market, " ", Spread, " ", TestCondition1, " ", strftime("%d-%m-%Y %H:%M:%S", gmtime()))
+    f = open(Market+".txt", 'a')
+    f.write(Market + " " + str(Spread) + " " + str(TestCondition1) + " " + strftime("%d-%m-%Y %H:%M:%S", gmtime()) + "\n")
+    f.close()
 
   time.sleep(60)
 except KeyboardInterrupt:
